7b7702ab632cf3c35e7b2b2abd17ec51151773ee
Marek Generowicz <generowicz@gmail.com>
2023-05-05 09:21:21 +0200

    Create configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,199 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  hardware.ledger.enable = true;
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "generowicz-nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nl_NL.UTF-8";
+    LC_IDENTIFICATION = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MONETARY = "nl_NL.UTF-8";
+    LC_NAME = "nl_NL.UTF-8";
+    LC_NUMERIC = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8";
+    LC_TELEPHONE = "nl_NL.UTF-8";
+    LC_TIME = "nl_NL.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.generowicz = {
+    isNormalUser = true;
+    description = "Generowicz";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+     
+      firefox
+      kate
+      vlc
+    #  thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   
+    wget
+    wsl-open
+    handlr 
+    curl
+    emacs
+    git
+    ripgrep  ripgrep-all  # better grep + find
+    fd                    # better find
+    bat                   # better cat
+    du-dust               # better du
+    exa                   # better ls
+    tldr                  # alternative to drinking from `man` fire hose
+    just        # recipe / command runner
+    hyperfine   # benchmarking
+    delta       # better diffs
+    skim        # fuzzy finder
+    rlwrap      # add readline capabilities to crappy shells
+    mosh        # more robust alternative to ssh
+    sd          # intuitive find and replace CLI
+    github-cli # git and git-lfs enabled below
+
+   # Golden oldies
+    gimp
+    file lsof
+    wget curl
+    tmux
+    rsync
+    xosview2 
+    amass
+    scrcpy
+    adb-sync 
+    procs bottom zenith
+   
+
+
+   #browser
+    brave
+    opera
+    ungoogled-chromium
+
+   #Communication
+    discord-canary
+    discord
+    slack
+    signal-desktop-beta
+    element-desktop
+   #Dev
+    vscode    
+    python3Full
+
+    scrcpy
+    kitty
+    ledger-live-desktop
+    monero-gui
+  ];
+
+  programs.htop = {
+    enable = true;
+    settings.cpu_count_from_zero = true;
+  };
+
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+  };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+}
